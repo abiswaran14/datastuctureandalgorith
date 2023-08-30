b9116d046f6cd2effce256e94721bcba9ceff1e6
abiswaran14 <abiswaran1404@gmail.com>
2023-08-30 21:52:45 +0530

    first commit

---
+++ project1.py
@@ -0,0 +1,52 @@
+
+Conversation opened. 1 unread message.
+
+Skip to content
+Using Gmail with screen readers
+1 of 7
+(no subject)
+Inbox
+
+abi developer <abideveloper2023@gmail.com>
+21:50 (0 minutes ago)
+to me
+
+print("welocme to quiz competion")
+
+print("*******************")
+
+q1="Who invented OOP?"
+print(q1)
+print("--------------------")
+print("1) Andrea Ferro","2) Adele Goldberg","3) Alan Kay","4) Dennis Ritchie")
+
+
+a1=int(input("answer:"))
+
+if (a1==3 ):
+    print("correct answer")
+elif(a1>=4):
+    print("enter correct answer,no answer not found")
+
+else:
+    print("the correct answer is 3,Alan Kay invented OOP, Andrea Ferro was a part of SmallTalk Development. Dennis invented C++ and Adele Goldberg was in team to develop SmallTalk but Alan actually had got rewarded for OOP")
+
+
+
+print("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+
+q2="Which is not a feature of OOP in general definitions?"
+print(q2)
+print("--------------------")
+print("1) Efficient Code","2)Code reusability  ","3) Modularity","4) Duplicate/Redundant data")
+
+
+a1=int(input("answer:"))
+
+if (a1==4 ):
+    print("correct answer")
+elif(a1>=4):
+    print("enter correct answer,no answer not found")
+
+else:
+    print("the correct answer is 4,Explanation: Duplicate/Redundant data is dependent on programmer and hence can’t be guaranteed by OOP. Code reusability is done using inheritance. Modularity is supported by using different code files and classes. Codes are more efficient because of features of OOP.")
